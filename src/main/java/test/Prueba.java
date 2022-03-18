d920aa26886314bf5df2becaaa65ccd0e0661ea5
Didier Ramirez <didierjavierr@gmail.com>
2022-03-18 11:50:48 -0500

    Se crean objetos tipo Respuesta y la clase Pregunta para utilizar dichos objetos

--- src/main/java/test/Prueba.java
+++ src/main/java/test/Prueba.java
@@ -4,7 +4,7 @@
  */
 package test;
 
-import com.concurso.softku.Respuesta;
+import com.concurso.softku.*;
 
 /**
  *
@@ -12,11 +12,15 @@ import com.concurso.softku.Respuesta;
  */
 public class Prueba {
     public static void main(String[] args) {
-    Respuesta respuestaA_Ronda1 = new Respuesta("El pollo", 'A' , false);
-    Respuesta respuestaB_Ronda1 = new Respuesta("El gallo", 'B' , false);
-    Respuesta respuestaC_Ronda1 = new Respuesta("La gallina", 'A' , true);
-    Respuesta respuestaD_Ronda1 = new Respuesta("La gallineta", 'B' , false);
+//    Respuesta respuestaA_Ronda1 = new Respuesta("El pollo", 'A' , false);
+//    Respuesta respuestaB_Ronda1 = new Respuesta("El gallo", 'B' , false);
+//    Respuesta respuestaC_Ronda1 = new Respuesta("La gallina", 'A' , true);
+//    Respuesta respuestaD_Ronda1 = new Respuesta("La gallineta", 'B' , false);
+//    Respuesta respuestaA_Pregunta2_Ronda1 = new Respuesta("Uno", 'A' , false);
+//    Respuesta respuestaB_Pregunta2_Ronda1 = new Respuesta("Todos", 'B' , true);
+//    Respuesta respuestaC_Pregunta2_Ronda1 = new Respuesta("Ninguno", 'A' , false);
+//    Respuesta respuestaD_Pregunta2_Ronda1 = new Respuesta("Siete", 'B' , false);
     
-        System.out.println(respuestaC_Ronda1);
+       // System.out.println(Pregunta.);
     }
 }
